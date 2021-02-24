052cb0d1464adbfe5787344dd57b85f10169937a
Andre Moreira <andre.moreira.9@hotmail.com>
2021-02-24 10:44:50 +0000

    Complete hero movement

--- practical/hero-p02/src/main/java/Game.java
+++ practical/hero-p02/src/main/java/Game.java
@@ -1,13 +1,20 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
 import java.io.IOException;
 
 public class Game {
+
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
     public Game() {
         try {
             TerminalSize tSize = new TerminalSize(40, 20);
@@ -24,11 +31,46 @@ public class Game {
         }
     }
 
+    private void processKey(KeyStroke key) throws IOException {
+        KeyType keyType = key.getKeyType();
+
+        switch (keyType) {
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowUp:
+                y--;
+                break;
+            case Character:
+                if (key.getCharacter() == 'q'){
+                    screen.close();
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + keyType);
+        }
+    }
+
     public void run() {
-        try  {
-            this.draw();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        while (true) {
+            try  {
+                KeyStroke key = screen.readInput();
+
+                if (key.getKeyType() == KeyType.EOF)
+                    break;
+
+                processKey(key);
+                this.draw();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -37,8 +79,4 @@ public class Game {
         this.screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         this.screen.refresh();
     }
-
-    private Screen screen;
-    private int x = 10;
-    private int y = 10;
 }
